56c7bf3eac97ae217848e9ae4ecfad91a0fcd2d7
WiiliamMentzer <william.mentzer2k@gmail.com>
2022-09-30 14:20:15 -0700

    WIP added cshtml files for Order and Vendor, updated Order and Vendor cs to include new variables

--- VendorOrder/Models/Order.cs
+++ VendorOrder/Models/Order.cs
@@ -5,6 +5,8 @@ namespace VendorOrder.Models
   public class Order
   {
     public string Description { get; set; }
+    public string Date { get; set; }
+    public int Price { get; set; }
     public int ID { get; }
     private static List<Order> _instances = new List<Order> { };
 
@@ -13,6 +15,16 @@ namespace VendorOrder.Models
       Description = description;
       _instances.Add(this);
       ID = _instances.Count;
+    }
+
+    public static List<Order> GetAllOrders()
+    {
+      return _instances;
+    }
+
+    public static Order FindOrder(int orderID)
+    {
+      return _instances[orderID-1];
     }
   }
 }
--- VendorOrder/Models/Vendor.cs
+++ VendorOrder/Models/Vendor.cs
@@ -6,15 +6,27 @@ namespace VendorOrder.Models
   {
     public static List<Vendor> _instances = new List<Vendor> {};
     public string vendorName { get; set; }
+    public string vendorDescription { get; set; }
     public int ID { get; }
     public List<Order> Order { get; set; }
 
-    public Vendor(string name)
+    public Vendor(string name, string description)
     {
       vendorName = name;
+      vendorDescription = description
       _instances.Add(this);
       ID = _instances.Count;
       Order = new List<Order>{};
+    }
+
+    public static List<Order> GetAllVendors()
+    {
+      return _instances;
+    }
+
+    public static Vendor Find(int vendorID)
+    {
+      return _instances[vendorID-1];
     }
   }
 }
